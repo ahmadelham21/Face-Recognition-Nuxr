dd9a6ab1758e4deb8cf67cf8d21baf38b4c9ff34
Ahmad Ilham <158115636+ahmadelham21@users.noreply.github.com>
2024-12-16 12:02:31 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,37 +3,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   plugins: [{ src: "~/plugins/faceLandmark.js", mode: "client" }],
-  build: {
-    transpile: [
-      '@tensorflow-models/face-landmarks-detection', 
-      '@tensorflow/tfjs-core',
-      '@tensorflow/tfjs-backend-webgl'
-    ]
-  },
-  vite: {
-    optimizeDeps: {
-      include: [
-        '@tensorflow-models/face-landmarks-detection',
-        '@tensorflow/tfjs-core',
-        '@tensorflow/tfjs-backend-webgl'
-      ],
-      esbuildOptions: {
-        target: 'es2020'
-      }
-    },
-    ssr: {
-      noExternal: [
-        '@tensorflow-models/face-landmarks-detection',
-        '@tensorflow/tfjs-core',
-        '@tensorflow/tfjs-backend-webgl'
-      ]
-    }
-  },
-  webpack: {
-    optimization: {
-      splitChunks: {
-        chunks: 'all'
-      }
-    }
-  }
+ 
+
 })
