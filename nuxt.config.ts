40ef70cfe0a6be33f5eef0e2ba687fab6027a924
Ahmad Ilham <158115636+ahmadelham21@users.noreply.github.com>
2024-12-16 12:26:40 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  plugins: [{ src: "~/plugins/faceLandmark.js", mode: "client" }],
+  plugins: [{ src: "~/plugins/face-landmark.js", mode: "client" }],
  
 
 })
